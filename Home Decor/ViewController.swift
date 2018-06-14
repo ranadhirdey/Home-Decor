0d326e6f25a5cd946ff7da321a8294a0f88eebb8
Ranadhir Dey <ranadhir.dey@intergraph.com>
2018-06-15 01:00:01 +0530

    Floor texture added.

--- Home Decor/ViewController.swift
+++ Home Decor/ViewController.swift
@@ -14,15 +14,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var sceneView: ARSCNView!
     let config = ARWorldTrackingConfiguration()
     
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        config.planeDetection = .horizontal
         sceneView.session.run(config)
-        let capsuleNode = SCNNode(geometry: SCNCapsule(capRadius: 0.03, height: 0.1))
-        capsuleNode.position = SCNVector3(0.1, 0.1, -0.1)
-        sceneView.scene.rootNode.addChildNode(capsuleNode)
+
+        sceneView.delegate = self
+        
+        
+        //        let capsuleNode = SCNNode(geometry: SCNCapsule(capRadius: 0.03, height: 0.1))
+        //        capsuleNode.position = SCNVector3(0.1, 0.1, -0.1)
+        //
+        //        capsuleNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue //1
+        //        capsuleNode.eulerAngles = SCNVector3(0,0,Double.pi/2)//2
+        //
+        //        sceneView.scene.rootNode.addChildNode(capsuleNode)
+
     }
+    
+    fileprivate func createFloorNode(anchor:ARPlaneAnchor) ->SCNNode{
+        let floorNode = SCNNode(geometry: SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))) //1
+        floorNode.position=SCNVector3(anchor.center.x,0,anchor.center.z)                                               //2
+        floorNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "WoodenFloorTile")                        //3
+        floorNode.geometry?.firstMaterial?.isDoubleSided = true                                                        //4
+        floorNode.eulerAngles = SCNVector3(Double.pi/2,0,0)                                                             //5
+        return floorNode                                                                                               //6
+    }
+    
+    
 
 }
 
 
+extension ViewController:ARSCNViewDelegate{
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let planeNode = createFloorNode(anchor: planeAnchor)
+        node.addChildNode(planeNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        node.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        let planeNode = createFloorNode(anchor: planeAnchor)
+        node.addChildNode(planeNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let _ = anchor as? ARPlaneAnchor else {return}
+        node.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+    }
+}
