3c6f9fbdbb9dd192cbca74975ccb201b05835c87
Ranadhir Dey <ranadhir.dey@intergraph.com>
2018-06-14 14:41:35 +0530

    first commit

---
+++ Home Decor/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Home Decor
+//
+//  Created by Ranadhir Dey on 14/06/18.
+//  Copyright © 2018 ARFactory. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let config = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
