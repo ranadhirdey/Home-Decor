9fc608aa84fb9b5c40fec59ff9d0db1b3b1ee334
Ranadhir Dey <ranadhir.dey@intergraph.com>
2018-06-24 21:40:26 +0530

    Tile collection added

--- Home Decor/ViewController.swift
+++ Home Decor/ViewController.swift
@@ -12,9 +12,16 @@ import ARKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var collectionView: UICollectionView!{
+        didSet{
+            collectionView.delegate = self
+            collectionView.dataSource = self
+        }
+    }
     let config = ARWorldTrackingConfiguration()
     
-
+    let floorImageArray = ["Wood1","Wood2","Wood3","Wood4", "Wood5", "Wood6", "Tile1", "Tile2", "Tile3", "Tile4"]
+    lazy var imageName = floorImageArray[0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +45,7 @@ class ViewController: UIViewController {
     fileprivate func createFloorNode(anchor:ARPlaneAnchor) ->SCNNode{
         let floorNode = SCNNode(geometry: SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))) //1
         floorNode.position=SCNVector3(anchor.center.x,0,anchor.center.z)                                               //2
-        floorNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "WoodenFloorTile")                        //3
+        floorNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: imageName)                                //3
         floorNode.geometry?.firstMaterial?.isDoubleSided = true                                                        //4
         floorNode.eulerAngles = SCNVector3(Double.pi/2,0,0)                                                             //5
         return floorNode                                                                                               //6
@@ -71,4 +78,58 @@ extension ViewController:ARSCNViewDelegate{
             node.removeFromParentNode()
         }
     }
+}
+
+extension ViewController:UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return floorImageArray.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        if let imgVw = cell.viewWithTag(1) as? UIImageView{
+            imgVw.image = UIImage(named: floorImageArray[indexPath.row])
+            resizeImageInCell(cell, isSelected: imageName == floorImageArray[indexPath.row])
+        }
+        
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let cell = self.collectionView.cellForItem(at: indexPath)
+        
+        resizeImageInCell(cell!, isSelected: true)
+        
+        
+        imageName = floorImageArray[indexPath.row]
+        collectionView.reloadData()
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = self.collectionView.cellForItem(at: indexPath)
+        
+        resizeImageInCell(cell!, isSelected: false)
+    }
+    
+    func resizeImageInCell(_ cell:UICollectionViewCell, isSelected:Bool){
+        
+        cell.layer.cornerRadius = isSelected ? 35 : 20
+        cell.layer.masksToBounds = true
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return imageName == floorImageArray[indexPath.row] ? CGSize(width: 70, height: 70) : CGSize(width: 40, height: 40)
+    }
+    
 }
